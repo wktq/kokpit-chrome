d5a2812433ad672103b03b0068980ada2a180d66
wktq <wakatsuki@commonize.jp>
2017-04-04 20:27:28 +0900

    add github repos

--- main.js
+++ main.js
@@ -11,8 +11,74 @@ firebase.initializeApp(config);
 var tasksRef = firebase.database().ref('tasks/');
 tasksRef.on('value', function(data) {
   data.forEach(function(childData) {
-    $(".collapsible").append('<li>' +
-      '<div class="collapsible-header"><i class="material-icons">filter_drama</i>' + childData.val().title + '</div>' +
-      '</li>');
+    console.log(childData.val().title);
   });
+});
+
+
+$(function() {
+  $('.auth-btn-github').click(function() {
+    OAuth.initialize('RiFT8OjQLXUWmCkGjdmQm-VyJLU');
+    OAuth.popup("github", function(err, res) {
+      res.get('/user/repos?access_token=' + res.access_token)
+        .done(function(result) {
+          $.each(result, function() {
+              console.log(this);
+
+             $('.repos').append('<a href="' + this.html_url + '" target="_blank">' +
+                                  '<div class="col s12 m3">' +
+                                    '<div class="card">' +
+                                      '<div class="card-content">' +
+                                        '<span class="card-title">' + this.name + '</span>' +
+                                        '<p>' + this.description + '</p>' +
+                                      '</div>' +
+                                      '<div class="card-tabs">' +
+                                        '<ul class="tabs tabs-fixed-width">' +
+                                          '<li class="tab"><a href="#test4">Test 1</a></li>' +
+                                          '<li class="tab"><a class="active" href="#test5">Test 2</a></li>' +
+                                          '<li class="tab"><a href="#test6">Test 3</a></li>' +
+                                        '</ul>' +
+                                      '</div>' +
+                                      '<div class="card-content grey lighten-4">' +
+                                        '<div id="test4">Test 1</div>' +
+                                        '<div id="test5">Test 2</div>' +
+                                        '<div id="test6">Test 3</div>' +
+                                      '</div>' +
+                                    '</div>' +
+                                  '</div>' +
+                                '</a>');
+          });
+        });
+        res.get('/user/repos/issues?access_token=' + res.access_token)
+          .done(function(result) {
+            console.log(result);
+            $.each(result, function() {
+
+            });
+          });
+      });
+
+    });
+
+  if (location.hash === '#callback') {
+    location.hash = '';
+    OAuth.callback('github')
+      .done(function(res) {
+        res.get('/user')
+          .done(function(res) {
+            var result = $('<div>')
+              .append($('<div>').text('Hello, ' + res.login + '!'))
+              .append($('<img>').attr('src', res.avatar_url));
+            $('#success').html(result);
+          })
+          .fail(function(res) {
+            window.alert('Failed to get usre info: see console for details');
+            console.dir(err);
+          })
+      })
+      .fail(function(err) {
+        window.alert('Failed to authorization: see console for details');
+        console.dir(err);
+      });
+  }
 });
