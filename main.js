8468b9ffe869df6c823a648a493c61bb3d6d86b0
wktq <wakatsuki@commonize.jp>
2017-04-05 00:43:21 +0900

    change

--- main.js
+++ main.js
@@ -16,49 +16,60 @@ tasksRef.on('value', function(data) {
 });
 
 
+
 $(function() {
-  $('.auth-btn-github').click(function() {
-    OAuth.initialize('RiFT8OjQLXUWmCkGjdmQm-VyJLU');
-    OAuth.popup("github", function(err, res) {
-      res.get('/user/repos?access_token=' + res.access_token)
-        .done(function(result) {
-          $.each(result, function() {
-              console.log(this);
+  var user = JSON.parse(localStorage.getItem("user_info"));
+  if (user) {
+    $('.auth-btn-github').after(user.name + 'としてログインしています！');
+    $('.auth-btn-github').hide();
 
-             $('.repos').append('<a href="' + this.html_url + '" target="_blank">' +
-                                  '<div class="col s12 m3">' +
-                                    '<div class="card">' +
-                                      '<div class="card-content">' +
-                                        '<span class="card-title">' + this.name + '</span>' +
-                                        '<p>' + this.description + '</p>' +
-                                      '</div>' +
-                                      '<div class="card-tabs">' +
-                                        '<ul class="tabs tabs-fixed-width">' +
-                                          '<li class="tab"><a href="#test4">Test 1</a></li>' +
-                                          '<li class="tab"><a class="active" href="#test5">Test 2</a></li>' +
-                                          '<li class="tab"><a href="#test6">Test 3</a></li>' +
-                                        '</ul>' +
-                                      '</div>' +
-                                      '<div class="card-content grey lighten-4">' +
-                                        '<div id="test4">Test 1</div>' +
-                                        '<div id="test5">Test 2</div>' +
-                                        '<div id="test6">Test 3</div>' +
-                                      '</div>' +
-                                    '</div>' +
-                                  '</div>' +
-                                '</a>');
-          });
+    $.ajax({
+      url: "https://api.github.com/user/repos",
+      type:'GET',
+      dataType: 'json',
+      data : {access_token : user.access_token},
+      timeout:10000,
+      success: function(data) {
+        $.each(data, function() {
+          console.dir(this);
+          addRepo(this);
         });
-        res.get('/user/repos/issues?access_token=' + res.access_token)
-          .done(function(result) {
-            console.log(result);
-            $.each(result, function() {
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        console.warn(errorThrown);
+      }
+    });
 
+  } else {
+    $('.auth-btn-github').click(function() {
+      OAuth.initialize('RiFT8OjQLXUWmCkGjdmQm-VyJLU');
+      OAuth.popup("github", function(err, res) {
+        var userInfo = new Object();
+        userInfo.access_token = res.access_token;
+
+        res.get('/user')
+        .done(function(result) {
+          userInfo.name = result.name;
+          localStorage.setItem('user_info', JSON.stringify(userInfo));
+        });
+
+        res.get('/user/repos?access_token=' + res.access_token)
+          .done(function(result) {
+            $.each(result, function() {
+              addRepo(result);
             });
           });
-      });
+          res.get('/user/repos/issues?access_token=' + res.access_token)
+            .done(function(result) {
+              console.log(result);
+              $.each(result, function() {
 
-    });
+              });
+            });
+        });
+
+      });
+    }
 
   if (location.hash === '#callback') {
     location.hash = '';
@@ -82,3 +93,28 @@ $(function() {
       });
   }
 });
+
+function addRepo(repo) {
+  $('.repos').append('<a href="' + repo.html_url + '" target="_blank">' +
+                       '<div class="col s12 m3">' +
+                         '<div class="card">' +
+                           '<div class="card-content">' +
+                             '<span class="card-title">' + repo.name + '</span>' +
+                             '<p>' + repo.description + '</p>' +
+                           '</div>' +
+                           '<div class="card-tabs">' +
+                             '<ul class="tabs tabs-fixed-width">' +
+                               '<li class="tab"><a href="#test4">Test 1</a></li>' +
+                               '<li class="tab"><a class="active" href="#test5">Test 2</a></li>' +
+                               '<li class="tab"><a href="#test6">Test 3</a></li>' +
+                             '</ul>' +
+                           '</div>' +
+                           '<div class="card-content grey lighten-4">' +
+                             '<div id="test4">Test 1</div>' +
+                             '<div id="test5">Test 2</div>' +
+                             '<div id="test6">Test 3</div>' +
+                           '</div>' +
+                         '</div>' +
+                       '</div>' +
+                     '</a>');
+}
